9b7e57c42469937d9747944bec544f31c85c6f98
wingmano <rafakortez@gmail.com>
2026-02-23 10:50:21 -0300

    fix: add missing config import in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-﻿export function calculateDurationInMinutes(
+﻿import { config } from "./config";
+
+export function calculateDurationInMinutes(
   openTime: number,
   closeTime: number,
 ): number {
